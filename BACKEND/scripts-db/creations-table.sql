cdb6eb20e152cd064211cffdf9d575a003501128
ArnaudW29 <64148403+ArnaudW29@users.noreply.github.com>
2020-04-26 19:13:57 +0200

    Update creations-table.sql

--- BACKEND/scripts-db/creations-table.sql
+++ BACKEND/scripts-db/creations-table.sql
@@ -14,7 +14,7 @@ creation de la table contenant toutes les informations de la recette */
 
 CREATE TABLE tbRhum (
  rhumId CHAR(3) NOT NULL, /*r01,r02*/
- rhumAjout DATE,   /*25 mars 21:20:21 2020 */
+ rhumAjout DATETIME,   /*31.12.99  */
  rhumPrix DECIMAL (4,2) NULL, /* 20,21, 19,20 */
  rhumIngre VARCHAR(255) NOT NULL, /* fraise, citron ..*/
  rhumMacer VARCHAR(30) NOT NULL,  /* 1 semaine , 1mois */ 
