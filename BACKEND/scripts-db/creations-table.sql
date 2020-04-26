5aa63b30ab86fd4aadb61acfb49c9af8db7f7100
ArnaudW29 <64148403+ArnaudW29@users.noreply.github.com>
2020-04-26 17:53:25 +0200

    Update creations-table.sql

--- BACKEND/scripts-db/creations-table.sql
+++ BACKEND/scripts-db/creations-table.sql
@@ -7,3 +7,16 @@ nomId char(3) NOT NULL,         /*rO1,rO2*/
 nomNom VARCHAR(40) NOT NULL,
 constraint pk_Noms PRIMARY KEY (nomId)
 );
+/* ARNAUD WAETERMANS 
+creation de la table contenant toutes les informations de la recette */
+
+CREATE TABLE tbRhum (
+ rhumId CHAR(3) NOT NULL, /*r01,r02*/
+ rhumAjout DATE,   /*25 mars 21:20:21 2020 */
+ rhumPrix DECIMAL (4,2) NULL, /* 20,21, 19,20 */
+ rhumIngre VARCHAR(255) NOT NULL, /* fraise, citron ..*/
+ rhumMacer VARCHAR(30) NOT NULL,  /* 1 semaine , 1mois */ 
+constraint pk_tbRhum PRIMARY KEY (rhumId),
+constraint fk_tbRhum_tbNoms FOREIGN KEY (rhumId) REFERENCES tbNoms (nomId)
+constraint fk_tbRhum_tbIngred FOREIGN KEY (rhumId) REFERENCES tbIngred (IngredId)
+);
