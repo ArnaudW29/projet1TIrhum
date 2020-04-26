9531cf1cb42c036ca7b9a01dbd4fc9a19a80c2f2
ArnaudW29 <64148403+ArnaudW29@users.noreply.github.com>
2020-04-26 17:33:01 +0200

    Update tbNoms.sql

--- BACKEND/scripts-db/creation-table/tbNoms.sql
+++ BACKEND/scripts-db/creation-table/tbNoms.sql
@@ -1,8 +1,8 @@
 /* ARNAUD WAETER%ANS 
 Creation de la table contenant les noms des rhums et leurs id */
-
 CREATE TABLE tbNoms (
-nomId char(3) NOT NULL,         /*rO1,rO2*/
-nomNom VARCHAR(40) NOT NULL,
-constraint pk_Noms PRIMARY KEY (nomId)
+    nomId char(3) NOT NULL,         /*rO1,rO2*/
+    nomNom VARCHAR(40) NOT NULL,
+    constraint pk_Noms PRIMARY KEY (nomId),
+    constraint fk_Noms_tbRhum FOREIGN KEY (nomId) REFERENCES tbRhum (rhumId)
 );
