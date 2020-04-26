8fa120379fcdb8fd176416f5a7e91d996a819c02
ArnaudW29 <64148403+ArnaudW29@users.noreply.github.com>
2020-04-26 17:12:11 +0200

    Delete creation-tables.sql

--- creation-tables.sql
+++
@@ -1,8 +0,0 @@
-/* ARNAUD WAETER%ANS 
-Creation de la table contenant les noms des rhums et leurs id */
-
-CREATE TABLE tbNoms (
-nomId char(3) NOT NULL,         /*rO1,rO2*/
-nomNom VARCHAR(40) NOT NULL,
-constraint pk_Noms PRIMARY KEY (nomId)
-);
