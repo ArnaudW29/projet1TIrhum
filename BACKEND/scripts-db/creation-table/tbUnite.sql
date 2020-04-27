6aa6ddb273294bd79dfea72f581327d1f829ae2d
ArnaudW29 <64148403+ArnaudW29@users.noreply.github.com>
2020-04-27 21:10:32 +0200

    Update tbUnite.sql

--- BACKEND/scripts-db/creation-table/tbUnite.sql
+++ BACKEND/scripts-db/creation-table/tbUnite.sql
@@ -1,6 +1,7 @@
-/* ARNAUD WAETERMANS */
+/* ARNAUD WAETERMANS 
+creation de la table avec les unites */
  CREATE TABLE tbUnite (
-	uniteId INTEGER NOT NULL auto_increment,
+	uniteId INTEGER NOT NULL DEFAULT AUTOINCREMENT,
 	uniteNom VARCHAR(15) NOT NULL,
 	uniteSymbole VARCHAR(3) NULL,
 	CONSTRAINT pk_tbUnite PRIMARY KEY (uniteId)
