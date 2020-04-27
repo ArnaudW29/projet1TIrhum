c7fcc8468536340b24010d8d89592bd3745e88d7
ArnaudW29 <64148403+ArnaudW29@users.noreply.github.com>
2020-04-27 16:39:26 +0200

    Update tbRhum.sql

--- BACKEND/scripts-db/creation-table/tbRhum.sql
+++ BACKEND/scripts-db/creation-table/tbRhum.sql
@@ -3,9 +3,14 @@ creation de la table contenant toutes les informations de la recette */
 
 CREATE TABLE tbRhum (
  rhumId CHAR(3) NOT NULL, /*r01,r02*/
- rhumAjout DATE,   /*25 mars 21:20:21 2020 */
- rhumPrix DECIMAL (4,2) NULL, /* 20,21, 19,20 */
- rhumIngre VARCHAR(255) NOT NULL, /* fraise, citron ..*/
- rhumMacer VARCHAR(30) NOT NULL,  /* 1 semaine , 1mois */ 
-constraint pk_tbRhum PRIMARY KEY (rhumId)
+ rhumMacer VARCHAR(30) NOT NULL,  /* 1 semaine , 1 mois */ 
+ rhumAjout DATETIME,   /*31.12.99  */
+ rhumPrix DECIMAL (4,2) NULL, /* 20.21, 19.20 */
+ rhumDeroulement VARCHAR(500) NULL, /* deroulement de la recette */ 
+ recetteId INT NOT NULL, /* 1 */
+ alcoolId INT NOT NULL, 
+constraint pk_tbRhum PRIMARY KEY (recetteId),
+CONSTRAINT fk_tbRhum_tbAlcool FOREIGN KEY (alcoolId) REFERENCES tbAlcool (alcoolId),
+constraint fk_tbRhum_tbNoms FOREIGN KEY (rhumId) REFERENCES tbNoms(nomId),
+ON DELETE RESTRICT 
 );
