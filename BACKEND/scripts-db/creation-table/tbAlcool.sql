4957fc48fc572ef4f863f1bce07348627ee5aa34
ArnaudW29 <64148403+ArnaudW29@users.noreply.github.com>
2020-04-27 21:08:54 +0200

    Update tbAlcool.sql

--- BACKEND/scripts-db/creation-table/tbAlcool.sql
+++ BACKEND/scripts-db/creation-table/tbAlcool.sql
@@ -1,6 +1,8 @@
-/* ARNAUD WAETERMANS */	
+/* ARNAUD WAETERMANS 
+creation de la table avec les alcools, degre , et nom */
 CREATE TABLE tbAlcool(
-	alcoolId INTEGER auto_increment NOT NULL,
+	alcoolId INTEGER  NOT NULL DEFAULT AUTOINCREMENT,
 	alcoolNom VARCHAR(50) NOT NULL,
-	alcoolDegre DECIMAL (3,1) NOT NULL
+	alcoolDegre DECIMAL (3,1) NOT NULL,
+	constraint pk_tbAlcool PRIMARY KEY (alcoolId)
 	);
