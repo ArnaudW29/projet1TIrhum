1baca1fb30f871d6c910ab0c3fa7cb85feeb30d1
ArnaudW29 <64148403+ArnaudW29@users.noreply.github.com>
2020-04-27 14:45:18 +0200

    Update creations-table.sql

--- BACKEND/scripts-db/creations-table.sql
+++ BACKEND/scripts-db/creations-table.sql
@@ -28,8 +28,9 @@ creation de la table contenant les ingredients et leurs prix pour chaque rhum */
 CREATE TABLE tbIngredient (
   ingreId        char(3) NOT NULL,
   ingrePrix       decimal(6,2) check(@col > 0.0) NULL ,
-  ingreNom    char(30) NOT  NULL,
-  ingreQte	INTEGER NOT NULL, 
+  ingreNom    varchar(30) NOT  NULL,
+  ingreQte	INTEGER NULL,
+  ingreCuill INTEGER NULL,
   CONSTRAINT pk__tbIngredient PRIMARY KEY (ingreId),
   CONSTRAINT fk__tbIngredient__tbRhum FOREIGN KEY (ingreId) REFERENCES tbRhum (rhumId),
   ON DELETE RESTRICT
