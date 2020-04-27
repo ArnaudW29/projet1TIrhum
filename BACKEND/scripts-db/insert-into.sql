5c301213d272c9907935204480df42679a0346d7
ArnaudW29 <64148403+ArnaudW29@users.noreply.github.com>
2020-04-27 21:07:08 +0200

    Update insert-into.sql

--- BACKEND/scripts-db/insert-into.sql
+++ BACKEND/scripts-db/insert-into.sql
@@ -1,28 +1,39 @@
 /* ARNAUD WAETERMANS 
 insertion des noms des rhums */
+
 INSERT INTO tbNoms(nomId, nomNom)
 VALUES 
-('r01', 'Rhum a la banane'),
-('r02', 'Rhum a la fraise et citron vert'),
-('r03', 'Rhum au piment oiseau'),
-('r04', 'Rhum aux bananes flambées'),
-('r05', 'Rhum au café'),
-('r06', 'Rhum au bounty');
+(1, 'Rhum a la banane'),
+(2, 'Rhum a la fraise et citron vert'),
+(3, 'Rhum au piment oiseau'),
+(4, 'Rhum aux bananes flambées'),
+(5, 'Rhum au café'),
+(6, 'Rhum au bounty')
 
 /* ARNAUD WAETERMANS 
 insertion des ingredients et prix pour les rhums */
+
 INSERT INTO tbIngredient (ingreId, ingreNom,)
 VALUES
 (1, 'banane' ),
 (2, 'de vanille'),
 (3, 'fraise' ),
-(4, 'citron vert'),
+(4, 'citron vert')
 /*  a continuer */
-INSERT INTO tbRecette_ingre (recetteId,ingreId,uniteId,quantité)
+
+
+/* ARNAUD WAETERMANS 
+insertion de tout les rhums disponibles */
+
+INSERT INTO tbAlcool(alcoolId, alcoolNom, alcoolDegre)
 VALUES 
-(1,1,5,5), /* 5 banane entiere dans le rhum 1 */
-(1,2,1,1) /* 1 gousse de vanille dans le rhum 1*/
-/*  a continuer */
+(1,'rhum agricole saint james',40),
+(2,'rhum agricole saint james',45),
+(3,'rhum agricole saint james',50)
+
+/* ARNAUD WAETERMANS 
+insertion des unites et symboles */
+
 INSERT INTO tbUnite(uniteId,uniteNom,uniteSymbole)
 VALUES 
 (1,'Gousse', NULL),
@@ -35,17 +46,18 @@ VALUES
 (8,'Degré', '°')
 /*  a continuer */
 
-/* ARNAUD WAETERMANS
-insertion  de toutes les informations concernant le rhum */
-INSERT INTO tbRhum (rhumId,recetteId,alcoolId,rhumAjout, rhumPrix, rhumMacer, rhumDeroulement)
+/* ARNAUD WAETERMANS */
+
+INSERT INTO tbRhum (nomId,recetteId,alcoolId,rhumAjout, rhumPrix, rhumMacer, rhumDeroulement)
 VALUES 
-('rO1',1, 1, 31.12.99, 7.12,  '3 mois', NULL)
+(1,1, 1, 1999-12-31, 7.12,  '3 mois', NULL)
 /* a continuer */
 
-/* ARNAUD WAETERMANS 
-insertion de tout les rhums disponibles */
-INSERT INTO tbAlcool(alcoolId, alcoolNom, alcoolDegre)
+/* ARNAUD WAETERMANS */
+
+INSERT INTO tbRecette_ingre (recetteId,ingreId,uniteId,quantité)
 VALUES 
-(1,'rhum agricole saint james',40)
-(2,'rhum agricole saint james',45)
-(3,'rhum agricole saint james',50)
+(1,1,5,5), /* 5 banane entiere dans le rhum 1 */
+(1,2,1,1) /* 1 gousse de vanille dans le rhum 1*/
+/*  a continuer */
+
