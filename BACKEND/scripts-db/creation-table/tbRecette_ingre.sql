1d81ab9013cc629d443a2644a369172227cb0673
ArnaudW29 <64148403+ArnaudW29@users.noreply.github.com>
2020-04-27 21:10:02 +0200

    Update tbRecette_ingre.sql

--- BACKEND/scripts-db/creation-table/tbRecette_ingre.sql
+++ BACKEND/scripts-db/creation-table/tbRecette_ingre.sql
@@ -1,10 +1,11 @@
-/* ARNAUD WAETERMANS */ 
+/* ARNAUD WAETERMANS 
+creation de la table de la recette */
 CREATE TABLE tbRecette_ingre (
 recetteId INT NOT NULL,
 ingreId  INT NOT NULL,
 uniteId INT NOT NULL,
 quantité INT NOT NULL, 
+CONSTRAINT fk_tbRecette_ingre_tbRhum FOREIGN KEY (recetteId) REFERENCES tbRhum (recetteId),
 CONSTRAINT fk_tbRecette_ingre_tbUnite FOREIGN key (uniteId) REFERENCES tbUnite (uniteId),
 CONSTRAINT fk_tbRecette_ingre_tbIngredient FOREIGN key (ingreId) REFERENCES tbIngredient(ingreId),
-CONSTRAINT fk_tbRecette_ingre_tbRhum FOREIGN KEY (recetteId) REFERENCES tbRhum (recetteId)
 );
