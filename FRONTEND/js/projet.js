3798062d0199823c8c87ba16081df5c370122587
ArnaudW29 <64148403+ArnaudW29@users.noreply.github.com>
2020-05-06 18:38:23 +0200

    Update projet.js

--- FRONTEND/js/projet.js
+++ FRONTEND/js/projet.js
@@ -1,1 +1,20 @@
+/* ARNAUD WAETERMANS */
+"use strict";
 
+function initNom(){
+	let xhr= new XMLHttpRequest();
+	xhr.open('get',"http://localhost:81/getNom", true);
+	xhr.onload = 
+		function x(){
+			let rep = JSON.parse(xhr.responseText);
+			let id= "";
+			let str="";
+			for(let i of rep){
+				id++;
+				str += "<option value='"+i.nomId+"'>"+ i.nomNom +"</option>";
+				console.log(id);
+			}
+			gid("rhumSel").innerHTML = str;
+		};
+	xhr.send();
+}
