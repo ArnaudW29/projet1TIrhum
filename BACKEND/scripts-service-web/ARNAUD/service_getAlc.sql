935b95306096044ed3d056e66537fcbcf6bf50ab
ArnaudW29 <64148403+ArnaudW29@users.noreply.github.com>
2020-05-07 11:49:41 +0200

    Update service_getAlc.sql

--- BACKEND/scripts-service-web/ARNAUD/service_getAlc.sql
+++ BACKEND/scripts-service-web/ARNAUD/service_getAlc.sql
@@ -5,7 +5,7 @@ BEGIN
        call sa_set_http_header('Content-Type','application,/json');
       
 SELECT  
-        A.alcoolId ,A.alcoolNom || ' ' || A.alcoolDegre
+        A.alcoolId ,A.alcoolNom || ' ' || A.alcoolDegre as NomDegr
 FROM    
     dba.tbAlcool as A
 ORDER BY A.alcoolDegre asc;
