6a30628cfe5d21e197166c10ab6ba0fcbf3ca78d
ArnaudW29 <64148403+ArnaudW29@users.noreply.github.com>
2020-05-07 12:10:11 +0200

    Create service-getUnite.sql

---
+++ BACKEND/scripts-service-web/ARNAUD/service-getUnite.sql
@@ -0,0 +1,17 @@
+
+/* ARNAUD WAETERMANS 
+select des unites  et de leurs  id  */ 
+CREATE PROCEDURE "dba"."getUnite"()
+BEGIN 
+       call sa_set_http_header('Content-Type','application,/json');
+      
+SELECT  
+        U.uniteId ,U.uniteNom || ' ' || U.uniteSymbole
+FROM    
+    dba.tbUnite as U
+ORDER BY U.uniteNom asc;
+ END;
+/* service */
+
+-------------------------------------------------------
+CREATE SERVICE "getUnite" TYPE 'JSON' AUTHORIZATION OFF USER "dba" URL ON METHODS 'GET' AS call dba.getUnite();
