dabcd8f7d5cc1865381da8f172fb19cbeeb8a9d5
ArnaudW29 <64148403+ArnaudW29@users.noreply.github.com>
2020-05-08 13:42:30 +0200

    Update projet.js

--- FRONTEND/js/projet.js
+++ FRONTEND/js/projet.js
@@ -38,17 +38,44 @@ function initUnite(){
 		for(let i of rep){
 			str += "<option value='"+i.uniteId+"'>"+ i.UniteSymbole+"</option>";
 		}
-		gid("uniteA").innerHTML = str;
 		gid("unite").innerHTML = str;
 		};
 	xhr.send();
 }
+function initIngre(){
+	let xhr = new XMLHttpRequest();
+	xhr.open('get',"http://localhost:81/getIngredient",true);
+	xhr.onload =
+		function x(){
+		let rep = JSON.parse(xhr.responseText);
+		let str="";
+		for(let i of rep){
+			str += "<option value='"+i.ingreId+"'>"+i.ingreNom+"</option>";
+		}
+		gid("ingredient").innerHTML=str;
+		}
+		xhr.send();
+}
 function ajoutIngre(){
 	ing++;
-	var elem = document.querySelector('#ingredient');
+	var elem = qs('#ingredient');
+	var elem2 = qs('#unite');
+	var elem3 = qs('#quantite');
 	var clone = elem.cloneNode(true);
+	var clone2 = elem2.cloneNode(true);
+	var clone3 = elem3.cloneNode(true);
+	clone.className ='ingredient';
 	clone.id = 'ingredient'+ing;
 	clone.name ='ingredient'+ing; // dans l'url sans ca on vois que ingredient ? peut etre enlever ?
 	elem.after(clone);
 	gid('ingredient'+ing).value ="";
+		clone2.className = 'ingredient';
+		clone2.id = 'unite'+ing;
+		clone.name ='unite'+ing;// dans l'url sans ca on vois que ingredient ? peut etre enlever ?
+		elem2.after(clone2);
+			clone3.className ='ingredient';
+			clone3.id = 'quantite'+ing;
+			clone3.name = 'quantite'+ing;
+			elem3.after(clone3);
+			gid('quantite'+ing).value ="";
 }
