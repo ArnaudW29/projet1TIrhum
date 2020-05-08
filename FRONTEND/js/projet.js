e209b37f162f5561fb90de44b9c0c53b1f7c7cf4
ArnaudW29 <64148403+ArnaudW29@users.noreply.github.com>
2020-05-08 12:22:34 +0200

    Update projet.js

--- FRONTEND/js/projet.js
+++ FRONTEND/js/projet.js
@@ -1,20 +1,54 @@
-/* ARNAUD WAETERMANS */
 "use strict";
-
+let ing="";
 function initNom(){
 	let xhr= new XMLHttpRequest();
 	xhr.open('get',"http://localhost:81/getNom", true);
 	xhr.onload = 
 		function x(){
 			let rep = JSON.parse(xhr.responseText);
-			let id= "";
 			let str="";
 			for(let i of rep){
-				id++;
 				str += "<option value='"+i.nomId+"'>"+ i.nomNom +"</option>";
-				console.log(id);
 			}
 			gid("rhumSel").innerHTML = str;
 		};
 	xhr.send();
+}
+function initAlc(){
+	let xhr= new XMLHttpRequest();
+	xhr.open('get',"http://localhost:81/getAlc",true);
+	xhr.onload =
+		function x(){
+		let rep = JSON.parse(xhr.responseText);
+		let str= "";
+		for(let i of rep){
+			str +=  "<option value='"+i.alcoolId+"'>"+ i.NomDegr+"</option>";
+		}
+		gid("alcool").innerHTML = str;
+		};
+		xhr.send();
+}
+function initUnite(){
+	let xhr = new XMLHttpRequest();
+	xhr.open('get',"http://localhost:81/getUnite",true);
+	xhr.onload =
+		function x(){
+		let rep = JSON.parse(xhr.responseText);
+		let str = "";
+		for(let i of rep){
+			str += "<option value='"+i.uniteId+"'>"+ i.UniteSymbole+"</option>";
+		}
+		gid("uniteA").innerHTML = str;
+		gid("unite").innerHTML = str;
+		};
+	xhr.send();
+}
+function ajoutIngre(){
+	ing++;
+	var elem = document.querySelector('#ingredient');
+	var clone = elem.cloneNode(true);
+	clone.id = 'ingredient'+ing;
+	clone.name ='ingredient'+ing; // dans l'url sans ca on vois que ingredient ? peut etre enlever ?
+	elem.after(clone);
+	gid('ingredient'+ing).value ="";
 }
