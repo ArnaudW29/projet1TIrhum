5c63099bf499a8f9907d703080cfd018895caba8
ArnaudW29 <64148403+ArnaudW29@users.noreply.github.com>
2020-05-09 15:22:55 +0200

    Update projet.js

--- FRONTEND/js/projet.js
+++ FRONTEND/js/projet.js
@@ -1,6 +1,7 @@
 "use strict";
 let ing="";
-
+let rep="";
+let ingr="";
 function initNom(){
 	let xhr= new XMLHttpRequest();
 	xhr.open('get',"http://localhost:81/getNom", true);
@@ -57,7 +58,25 @@ function initIngre(){
 		};
 		xhr.send();
 }
-function ajoutIngre(){
+function initRecette(){
+	let xhr= new XMLHttpRequest();
+	xhr.open('get',"http://localhost:81/initRecette", true );
+	xhr.onload =
+		function x(){
+			rep = JSON.parse(xhr.responseText);
+		};
+	xhr.send();
+}
+function initIngreRec(){
+	let xhr= new XMLHttpRequest();
+	xhr.open('get',"http://localhost:81/getRecette",true );
+	xhr.onload =
+		function x(){
+		ingr = JSON.parse(xhr.responseText);
+		};
+	xhr.send();
+}
+function ajoutIngre(){ //fonction qui permet de creer plus de champs d'input quand on clique sur un boutton
 	ing++;
 	let elem = qs('#ingredient');
 	let elem2 = qs('#unite');
@@ -79,31 +98,31 @@ function ajoutIngre(){
 			clone3.name = 'quantite'+ing;// dans l'url sans ca on vois que ingredient ? peut etre enlever ?
 			elem3.after(clone3);
 			gid('quantite'+ing).value ="";
-
 }
-let rep="";
-
-function initRecette(){
-	let xhr= new XMLHttpRequest();
-	xhr.open('get',"http://localhost:81/initRecette", true );
-	xhr.onload =
-		function x(){
-			 rep = JSON.parse(xhr.responseText);
-		};
-	xhr.send();
-}
-function affiRecette(){
+function affiRecette(){ // fonction qui sert a afficher les informations quand on selectionne un rhum dans une liste deroulante avec un onchange
    let xx = gid('rhumSel').value ;
    let y =  gid('rhum'+xx).innerText;
    gid('rhum').innerHTML = "<h2> Voici la liste d'ingrédient et la méthodologie pour le " + '<b>'+y + " </b> : </h2>";
    for(let x in rep ){
    	if(rep[x].nomNom === y ){
-   		gid('alcool').innerHTML = "Alcool utilsé : "+ '<b>' +rep[x].rhumQte + " "+ rep[x].rhumUnite + "  de " +rep[x].alcoolNom + " " + rep[x].alcoolDegre+ " °"+ '</b>';
-   		gid('maceration').innerHTML = "Le temps de maceration de ce rhum est de :" +'<b>' + rep[x].rhumMacer + '</b>';
-   		gid('prix').innerHTML = "Le prix du rhum est de : " + '<b>' + rep[x].rhumPrix + '</b>' + " €";
-   		gid('deroulement').innerHTML = "Une description et aide pour effectuer la recette : " + '<b>' + rep[x].rhumDeroulement + '</b>';
+   		gid('alcool').innerHTML = "Alcool utilsé : "+ '<br>'  + '<b>' +rep[x].rhumQte + " "+ rep[x].rhumUnite + "  de " +rep[x].alcoolNom + " " + rep[x].alcoolDegre+ " °"+ '</b>';
+   		gid('maceration').innerHTML = "Le temps de maceration de ce rhum est de :"+ '<br>'  +'<b>' + rep[x].rhumMacer + '</b>';
+   		gid('prix').innerHTML = "Le prix du rhum est de : "+ '<br>'  + '<b>' + rep[x].rhumPrix + '</b>' + " €";
+   		gid('deroulement').innerHTML = "Une description et aide pour effectuer la recette : "+ '<br>'  + '<b>' + rep[x].rhumDeroulement + '</b>';
    	}
    }
-   	console.log(y);
-	console.log(xx);
 }
+function affiIngredient(){ // fonction qui sert a afficher la liste des ingredients pour chaque rhums
+	let xx = gid('rhumSel').value ;
+	let y =  gid('rhum'+xx).innerText;
+	for(let x in ingr){
+		if(ingr[x].nomNom === y){
+			gid('ingredient').innerHTML += '<br>' + " "+'<b>'+  ingr[x].ingreNom +"  "+  ingr[x].quantité +"  "+ingr[x].uniteNom +'</b>';
+			console.log(xx+" ingredient")
+		}
+	}
+}
+function cleanIngre(){ // sert uniquement a vider la zone ingredient a chaque changement de selectionne pour eviter une accumulation d'ingredients residuels 
+	gid('ingredient').innerHTML = " Voici la liste des ingredients : "
+}
+
