a904069e1c9ffe98058085c276381f504acb2302
ArnaudW29 <64148403+ArnaudW29@users.noreply.github.com>
2020-05-11 00:23:10 +0200

    Update projet.js

--- FRONTEND/js/projet.js
+++ FRONTEND/js/projet.js
@@ -3,17 +3,28 @@ let ing="";
 let rep="";
 let ingr="";
 let ingreId="";
+let nomId="";
 function initNom(){
 	let xhr= new XMLHttpRequest();
 	xhr.open('get',"http://localhost:81/getNom", true);
 	xhr.onload = 
 		function x(){
 			let rep = JSON.parse(xhr.responseText);
+			nomId = JSON.parse(xhr.responseText);
 			let str="";
 			for(let i of rep){
 				str += "<option value='"+i.nomId+"'  id='"+"rhum"+i.nomId+"'>"+ i.nomNom +"</option>";
 			}
 			gid("rhumSel").innerHTML = str;
+		};
+	xhr.send();
+}
+function initNom2(){
+	let xhr= new XMLHttpRequest();
+	xhr.open('get',"http://localhost:81/getNom", true);
+	xhr.onload =
+		function x(){
+			nomId = JSON.parse(xhr.responseText);
 		};
 	xhr.send();
 }
@@ -114,32 +125,30 @@ function affiRecette(){ // fonction qui sert a afficher les informations quand o
    	}
    }
 }
-function affiRecetteId(){ //affiche l'id d'une recette selectionner avec un onchange
+function affiRecetteId(id){ //affiche l'id d'une recette selectionner avec un onchange
 	let xx = gid('rhumSel').value ;
-	gid('idrecette').value = xx;
+	gid(id).value = xx;
 }
-function affiRecetteIdReverse() {
-	let xx = gid('idrecette').value;
+function affiRecetteIdReverse(id) {
+	let xx = gid(id).value;
 	gid('rhumSel').value = xx
 }
-function maxId() {
+function maxId(id) {
 	let max="";
 	for(let x in rep ){
 		max++
 	}
-	var input = gid('idrecette');
+	var input = gid(id);
 	input.setAttribute("max",max);
 }
 function minId() {
 	let min="";
-	for(let x in rep ){
+	for(let x in nomId ){
 		min++
 	}
 	min += 1;
 	console.log(min);
 	var input = gid('idnom');
-	var input2= gid('idrecette');
-	input2.setAttribute("min",min);
 	input.setAttribute("min",min);
 }
 function minIdIngre(){
@@ -155,7 +164,7 @@ function idRecetteChanger(input1) {
 	gid('idrecette').value = input1.value;
 }
 function idRecetteChanger2(input2) {
-	gid('idnom').value = input2.value;
+	gid('idrhum').value = input2.value;
 }
 function affiIngredient(){ // fonction qui sert a afficher la liste des ingredients pour chaque rhums
 	let xx = gid('rhumSel').value ;
@@ -170,15 +179,14 @@ function affiIngredient(){ // fonction qui sert a afficher la liste des ingredie
 function cleanIngre(){ // sert uniquement a vider la zone ingredient a chaque changement de selection pour eviter une accumulation d'ingredients residuels
 	gid('ingredient').innerHTML = " Voici la liste des ingredients : "
 }
-function envoisIngre(nom,nomId,recetteId,alcoolId,rhumUnite,rhumQte,ingreId,uniteId,quantité,rhumMacer,rhumPrix,rhumDeroulement){
+function envoisIngre(nom,nomId){
 	let xhr = new XMLHttpRequest();
-//	let url = service + "?name=" + nomNom + "&alcool=" + alcoolId + "&uniteA=" + rhumUnite + "&quantiteA=" + rhumQte+ "&ingredient="+ingreId + "&unite=" + uniteId + "&quantite=" + quantité + "&temps=" + rhumMacer + "&prix=" + rhumPrix + "&explication=" + rhumDeroulement;
-	let url = `proc_insert_rhum?name=${nom}&idnom=${nomId}&idrecette=${recetteId}&alcool=${alcoolId}&uniteA=${rhumUnite}quantiteA=${rhumQte}&ingredient=${ingreId}&unite=${uniteId}&quantite=${quantité}&temps=${rhumMacer}&prix=${rhumPrix}&explication=${rhumDeroulement}`
-	console.log(url)
+	let url = `proc_insert_rhum?name=${nom}&idnom=${nomId}`;
+	console.log(url);
 	xhr.onload= function(){
 		console.log(url);
 	};
-	xhr.open('get', `http://localhost81/proc_insert_rhum?name=${nom}&idnom=${nomId}&idrecette=${recetteId}&alcool=${alcoolId}&uniteA=${rhumUnite}quantiteA=${rhumQte}&ingredient=${ingreId}&unite=${uniteId}&quantite=${quantité}&temps=${rhumMacer}&prix=${rhumPrix}&explication=${rhumDeroulement}`, true);
+	xhr.open('get', `proc_insert_rhum?name=${nom}&idnom=${nomId}`, true);
 	xhr.onerror = function(){
 		console.log("erreur")
 	};
@@ -207,19 +215,21 @@ function envoisIngre3(ingredient,idingredient){
 	};
 	xhr.send();
 }
+function envoisIngre4(idnom,idrecette,alcool,quantiteA,uniteA,temps,prix,explication){
+	let xhr = new XMLHttpRequest();
+	let url = 'proc_insert_rhumModif?idnom='+idnom+'&idrecette='+idrecette+'&alcool='+alcool+'&uniteA='+uniteA+'&quantiteA='+quantiteA+'&temps='+temps+'&prix='+prix +'&explication='+explication;
+	xhr.onload= function() {
+		console.log(url);
+	};
+	xhr.open('get','proc_insert_rhumModif?idnom='+idnom+'&idrecette='+idrecette+'&alcool='+alcool+'&quantiteA='+quantiteA+'&uniteA='+uniteA+'&temps='+temps+'&prix='+prix +'&explication='+explication,true)
+	xhr.onerror = function (){
+		console.log("xhr error");
+	};
+	xhr.send();
+}
 function submitForm(){
 	envoisIngre(gid('name').value,
-		gid('idnom').value,
-		gid('idrecette').value,
-		gid('alcool').value,
-		gid('uniteA').value,
-		gid('quantiteA').value,
-		gid('ingredient').value,
-		gid('unite').value,
-		gid('quantite').value,
-		gid('temps').value,
-		gid('prix').value,
-		gid('explication').value)
+		gid('idnom').value)
 }
 function submitForm2(){
 	envoisIngre2(gid('quantite').value,
@@ -231,4 +241,13 @@ function submitForm3(){
 	envoisIngre3(gid('ingredient2').value,
 		gid('ingredientId').value)
 }
-
+function submitForm4(){
+	envoisIngre4(gid('idrecette').value,
+		gid('idrhum').value,
+		gid('alcool').value,
+		gid('quantiteA').value,
+		gid('uniteA').value,
+		gid('temps').value,
+		gid('prix').value,
+		gid('explication').value)
+}
