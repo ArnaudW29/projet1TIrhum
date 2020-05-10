7d7e559b8cf828428fe1222ead918a14cb294b3d
ArnaudW29 <64148403+ArnaudW29@users.noreply.github.com>
2020-05-10 16:49:52 +0200

    Update webservice-insert-rhum.sql

--- BACKEND/scripts-api-rest/webservice-insert-rhum.sql
+++ BACKEND/scripts-api-rest/webservice-insert-rhum.sql
@@ -12,5 +12,20 @@ BEGIN
   (ingreId,uniteId, quantité);
   END;
   
+  ALTER PROCEDURE "DBA"."proc_insert_rhum"(in n varchar(40),in alcool integer, in uniteA varchar(10), in quantiteA integer,in ingredient integer, in unite integer,in quantite integer, in temps varchar(30),in prix decimal(4,2), in explication varchar(500))
+BEGIN
+  INSERT INTO tbNoms (nomNom)
+  VALUES 
+  (n);
+  INSERT INTO tbRhum (alcoolId,rhumQte,rhumUnite,rhumMacer,rhumPrix,rhumDeroulement)
+  VALUES 
+  (alcool,quantiteA,uniteA,temps,prix,explication);
+  INSERT INTO dba.tbRecette_ingre(ingreId,uniteId,quantité)
+  VALUES 
+  (ingredient,unite, quantite);
+  END;
+
+
+  
   ----------------------------------------------------------------------------------------
   CREATE SERVICE proc_insert_rhum TYPE 'RAW' AUTHORIZATION OFF USER "dba" METHODS 'GET' AS call dba.proc_insert_rhum(:nomNom,:alcoolId,:rhumUnite,:rhumQte,:ingreId,:uniteId,:quantité,:rhumMacer,:rhumPrix,:rhumDeroulement);
