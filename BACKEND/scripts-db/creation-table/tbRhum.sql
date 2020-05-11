71716817969110242df8a144da899d87e36fc838
ArnaudW29 <64148403+ArnaudW29@users.noreply.github.com>
2020-05-11 11:26:18 +0200

    Update tbRhum.sql

--- BACKEND/scripts-db/creation-table/tbRhum.sql
+++ BACKEND/scripts-db/creation-table/tbRhum.sql
@@ -1,13 +1,13 @@
-/* ARNAUD WAETERMANS 
-creation de la table du rhum */
 CREATE TABLE tbRhum (
- nomId INT  NOT NULL DEFAULT AUTOINCREMENT, 
+ nomId INT  NOT NULL, 
  recetteId INT NOT NULL, /* 1 */
  alcoolId INT NOT NULL, /* 1 */
+ rhumQte INT NOT NULL,
+ rhumUnite varchar(10) NOT NULL,
  rhumMacer VARCHAR(30) NOT NULL,  /* 1 semaine , 1 mois */ 
  rhumAjout DATETIME,   /*31.12.99  */
  rhumPrix DECIMAL (4,2) NULL, /* 20.21, 19.20 */
- rhumDeroulement VARCHAR(500) NULL, /* deroulement de la recette */ 
+ rhumDeroulement VARCHAR(500) NULL, /* deroulement de la recette */
 constraint pk_tbRhum PRIMARY KEY (recetteId),
 CONSTRAINT fk_tbRhum_tbAlcool FOREIGN KEY (alcoolId) REFERENCES tbAlcool (alcoolId),
 constraint fk_tbRhum_tbNoms FOREIGN KEY (nomId) REFERENCES tbNoms(nomId)
