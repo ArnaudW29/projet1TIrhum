66555a8a9a0be83bb5173efdac0dc0a7bb8db586
ArnaudW29 <64148403+ArnaudW29@users.noreply.github.com>
2020-05-11 11:20:35 +0200

    Update tbNoms.sql

--- BACKEND/scripts-db/creation-table/tbNoms.sql
+++ BACKEND/scripts-db/creation-table/tbNoms.sql
@@ -3,7 +3,7 @@
 Creation de la table contenant les noms des rhums et leurs id */
 
 CREATE TABLE tbNoms (
-nomId INT  NOT NULL DEFAULT AUTOINCREMENT,         
+nomId INT  NOT NULL ,         
 nomNom VARCHAR(40) NOT NULL,    /* rhum a la fraise , a la banane */
 constraint pk_Noms PRIMARY KEY (nomId)
 );
