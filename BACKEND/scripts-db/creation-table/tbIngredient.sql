f433fbdff38f63c1dd1b30cbb1a1de46e852ab31
ArnaudW29 <64148403+ArnaudW29@users.noreply.github.com>
2020-05-11 11:24:29 +0200

    Update tbIngredient.sql

--- BACKEND/scripts-db/creation-table/tbIngredient.sql
+++ BACKEND/scripts-db/creation-table/tbIngredient.sql
@@ -1,7 +1,8 @@
 /* ARNAUD WAETERMANS
 creation de la table contenant les ingredients pour chaque rhum */ 
 CREATE TABLE tbIngredient (
-  ingreId    INT NOT NULL  DEFAULT AUTOINCREMENT,
+  ingreId    INT NOT NULL ,
   ingreNom    varchar(30) NOT  NULL ,
   CONSTRAINT pk__tbIngredient PRIMARY KEY (ingreId),
+  CONSTRAINT "pk_tbIngredientNom" UNIQUE ( "ingreNom" ASC )
   );
