5fe2bff86d3e062bd3a5be4e1563d4ba459026ec
ArnaudW29 <64148403+ArnaudW29@users.noreply.github.com>
2020-05-11 11:34:49 +0200

    Update insert-into.sql

--- BACKEND/scripts-db/insert-into.sql
+++ BACKEND/scripts-db/insert-into.sql
@@ -6,9 +6,6 @@ VALUES
 (1, 'Rhum a la banane'),
 (2, 'Rhum a la fraise et citron vert'),
 (3, 'Rhum au piment oiseau'),
-(4, 'Rhum aux bananes flambées'),
-(5, 'Rhum au café'),
-(6, 'Rhum au bounty')
 
 /* ARNAUD WAETERMANS 
 insertion des ingredients et prix pour les rhums */
@@ -16,9 +13,11 @@ insertion des ingredients et prix pour les rhums */
 INSERT INTO tbIngredient (ingreId, ingreNom)
 VALUES
 (1, 'banane' ),
-(2, 'de vanille'),
+(2, 'vanille'),
 (3, 'fraise' ),
-(4, 'citron vert')
+(4, 'citron vert'),
+(5, 'gingembre'),
+(6,'piment oiseau')
 /*  a continuer */
 
 
@@ -43,14 +42,16 @@ VALUES
 (5,'entiere', NULL),
 (6,'Litre','L'),
 (7,'Centilitre', 'cl'),
-(8,'Degré', '°')
+(8,'entier',NULL)
 /*  a continuer */
 
 /* ARNAUD WAETERMANS */
 
-INSERT INTO tbRhum (nomId,recetteId,alcoolId,rhumQte,rhumUnite,rhumAjout, rhumPrix, rhumMacer, rhumDeroulement)
+INSERT INTO tbRhum (nomId,recetteId,alcoolId,rhumQte,rhumUnite, rhumPrix, rhumMacer, rhumDeroulement)
 VALUES 
-(1,1, 1,75,'cl', 1999-12-31, 7.12,  '3 mois', NULL)
+(1,1, 1,75,'cl', 7.12,  '3 mois','couper les bananes en tranches et les inserer dans un fond de rhum ajouter une gousse de vanille couper en tranche'),
+(2,2, 2,1,'L',10,'2 mois', 'couper les fraises en tranches presser les citrons vert pour y mettre le jus'),
+(3,3,1,75,'cl',7,32,' 1 semaine', 'faire des petits trous dans les piments puis les mettres dans le rhum ajouter du gingembre couper en tranche')
 /* a continuer */
 
 /* ARNAUD WAETERMANS */
@@ -58,6 +59,11 @@ VALUES
 INSERT INTO tbRecette_ingre (recetteId,ingreId,uniteId,quantité)
 VALUES 
 (1,1,5,5), /* 5 banane entiere dans le rhum 1 */
-(1,2,1,1) /* 1 gousse de vanille dans le rhum 1*/
+(1,2,1,1), /* 1 gousse de vanille dans le rhum 1*/
+(2,3,2,200),
+(2,4,7,50),
+(3,6,8,8),
+(3,5,2,35)
+
 /*  a continuer */
 
