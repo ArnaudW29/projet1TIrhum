9e71bf5ff2b498035a8e77a28cae699d96d5ff84
ArnaudW29 <64148403+ArnaudW29@users.noreply.github.com>
2020-05-11 11:45:26 +0200

    Update creations-table.sql

--- BACKEND/scripts-db/creations-table.sql
+++ BACKEND/scripts-db/creations-table.sql
@@ -5,7 +5,9 @@ Creation de la table contenant les noms des rhums et leurs id */
 CREATE TABLE tbNoms (
 nomId INT  NOT NULL,         
 nomNom VARCHAR(40) NOT NULL,    /* rhum a la fraise , a la banane */
-constraint pk_Noms PRIMARY KEY (nomId)
+constraint pk_Noms PRIMARY KEY (nomId),
+CONSTRAINT "pk_tbNoms" UNIQUE ( "nomNom" ASC );
+
 );
 
 /* ARNAUD WAETERMANS
